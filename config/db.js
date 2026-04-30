4c0a73215b780cc56c4c2a87450af4ada1c3e521
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-04-30 21:21:03 +0300

    .

--- config/db.js
+++ config/db.js
@@ -3,11 +3,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  "autopartsdatabase",
+  "autoparts",
+  "StrongPass123!",
+  {
     host: "127.0.0.1",
     port: 3306,
+    dialect: "mysql",
     logging: false,
-});
+  }
+);
 
 sequelize.authenticate()
     .then(() => console.log("✅ Connected to MySQL successfully!"))
