e968e654604b0c5c7433dec77787c42387461512
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-04-30 21:16:37 +0300

    .

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 const sequelize = new Sequelize(
     process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "localhost",
-    dialect: "mysql",
+    host: "127.0.0.1",
+    port: 3306,
     logging: false,
 });
 
